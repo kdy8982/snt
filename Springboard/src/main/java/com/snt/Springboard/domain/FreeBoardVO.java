2ecfb0165ff76ba4482472d333ee5365406ee0bb
Howon <Howon@SNT-PC>
2019-12-10 13:30:19 +0900

    board 공지 자유 게시판 VO 추가

---
+++ Springboard/src/main/java/com/snt/Springboard/domain/FreeBoardVO.java
@@ -0,0 +1,86 @@
+package com.snt.Springboard.domain;
+
+public class FreeBoardVO {
+	private String free_board_id;
+	private String free_board_title;
+	private String free_board_content;
+	private String free_board_name;
+	private String free_board_create_date;
+	private String department_id;
+	private String employee_id;
+	private String department_name;
+	private String employee_name;
+
+	public String getFree_board_id() {
+		return free_board_id;
+	}
+
+	public void setFree_board_id(String free_board_id) {
+		this.free_board_id = free_board_id;
+	}
+
+	public String getFree_board_title() {
+		return free_board_title;
+	}
+
+	public void setFree_board_title(String free_board_title) {
+		this.free_board_title = free_board_title;
+	}
+
+	public String getFree_board_content() {
+		return free_board_content;
+	}
+
+	public void setFree_board_content(String free_board_content) {
+		this.free_board_content = free_board_content;
+	}
+
+	public String getFree_board_name() {
+		return free_board_name;
+	}
+
+	public void setFree_board_name(String free_board_name) {
+		this.free_board_name = free_board_name;
+	}
+
+	public String getFree_board_create_date() {
+		return free_board_create_date;
+	}
+
+	public void setFree_board_create_date(String free_board_create_date) {
+		this.free_board_create_date = free_board_create_date;
+	}
+
+	public String getDepartment_id() {
+		return department_id;
+	}
+
+	public void setDepartment_id(String department_id) {
+		this.department_id = department_id;
+	}
+
+	public String getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(String employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+
+	public String getEmployee_name() {
+		return employee_name;
+	}
+
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+
+}
---
+++ Springboard/src/main/java/com/snt/Springboard/domain/NoticeBoardVO.java
@@ -0,0 +1,86 @@
+package com.snt.Springboard.domain;
+
+public class NoticeBoardVO {
+	private String notice_board_id;
+	private String notice_board_title;
+	private String notice_board_content;
+	private String notice_board_name;
+	private String notice_board_create_date;
+	private String department_id;
+	private String employee_id;
+	private String department_name;
+	private String employee_name;
+
+	public String getNotice_board_id() {
+		return notice_board_id;
+	}
+
+	public void setNotice_board_id(String notice_board_id) {
+		this.notice_board_id = notice_board_id;
+	}
+
+	public String getNotice_board_title() {
+		return notice_board_title;
+	}
+
+	public void setNotice_board_title(String notice_board_title) {
+		this.notice_board_title = notice_board_title;
+	}
+
+	public String getNotice_board_content() {
+		return notice_board_content;
+	}
+
+	public void setNotice_board_content(String notice_board_content) {
+		this.notice_board_content = notice_board_content;
+	}
+
+	public String getNotice_board_name() {
+		return notice_board_name;
+	}
+
+	public void setNotice_board_name(String notice_board_name) {
+		this.notice_board_name = notice_board_name;
+	}
+
+	public String getNotice_board_create_date() {
+		return notice_board_create_date;
+	}
+
+	public void setNotice_board_create_date(String notice_board_create_date) {
+		this.notice_board_create_date = notice_board_create_date;
+	}
+
+	public String getDepartment_id() {
+		return department_id;
+	}
+
+	public void setDepartment_id(String department_id) {
+		this.department_id = department_id;
+	}
+
+	public String getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(String employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+
+	public String getEmployee_name() {
+		return employee_name;
+	}
+
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+
+}
